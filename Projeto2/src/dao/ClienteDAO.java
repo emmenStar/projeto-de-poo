5b34d8e00eec0e84316e2b25e9aee0ca1a810f38
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-11-08 02:38:36 -0300

    ClienteDAO.java

--- Projeto2/src/dao/ClienteDAO.java
+++ Projeto2/src/dao/ClienteDAO.java
@@ -1,6 +1,5 @@
 package dao;
 
-import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,85 +10,43 @@ import java.util.List;
 import model.Cliente;
 
 public class ClienteDAO {
-    public static boolean adicionarCliente(Cliente cliente){
-        String sql = "INSERT INTO Cliente (nomeCliente, nomeProduto, data, valor) VALUES(?, ?, ?, ?)";
-        try{
-            Connection conexao = FabricaDeConexao.getConexao();
-            PreparedStatement stmt = conexao.prepareStatement(sql);
+    public static boolean adicionarCliente(Cliente cliente) {
+        String sql = "INSERT INTO Cliente (nomeCliente, nomeProduto, dataCompra, valorProduto) VALUES (?, ?, ?, ?)";
+        try (Connection conexao = FabricaDeConexao.getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setString(1, cliente.getNomeCliente());
             stmt.setString(2, cliente.getNomeProduto());
-            stmt.setInt(3, cliente.getData());
-            stmt.setDouble(4, cliente.getValor());
+            stmt.setLong(3, cliente.getDataCompra());
+            stmt.setDouble(4, cliente.getValorProduto());
             stmt.execute();
-            conexao.close();
             return true;
-        }catch (SQLException ex){
-            System.out.println(ex.toString());            
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+            return false;
         }
-        return false;
     }
 
-    // Criar os métodos para atualizar, deletar, listar por atributos
+    // Implemente os métodos para atualizar, deletar, listar por atributos aqui
 
-    public static List<Cliente> listarClientes(){
+    public static List<Cliente> listarClientes() {
         List<Cliente> clientes = new ArrayList<>();
-        String sql = "SELECT codigo, nomeCliente, nomeProduto, data, valor "
-                + "FROM Cliente";
-        try{
-            Connection conexao = FabricaDeConexao.getConexao();
-            if (conexao == null){
-                System.println("Sem conexao");
-                System.exit(0);
-            }
-            Statement stmt = conexao.createStatement();
-            System.out.println("Statement criada!");
-            ResultSet rs = stmt.executeQuery(sql);
-            System.out.println("Query executada!");
-            while (rs.next()){
+        String sql = "SELECT codigo, nomeCliente, nomeProduto, dataCompra, valorProduto FROM Cliente";
+        try (Connection conexao = FabricaDeConexao.getConexao();
+             PreparedStatement stmt = conexao.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
                 int codigo = rs.getInt("codigo");
                 String nomeCliente = rs.getString("nomeCliente");
                 String nomeProduto = rs.getString("nomeProduto");
-                int data = rs.getInt("data");
-                double valor = rs.getDouble("valor");
-                Cliente cliente = new Cliente(codigo, nomeCliente, nomeProduto, data, valor);
+                long dataCompra = rs.getLong("dataCompra");
+                double valorProduto = rs.getDouble("valorProduto");
+                Cliente cliente = new Cliente(codigo, nomeCliente, nomeProduto, dataCompra, valorProduto);
                 clientes.add(cliente);
                 System.out.println(cliente.toString());
             }
-            conexao.close();
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println(ex.toString());
         }
         return clientes;
     }
-    
-    public static List<Cliente> listarClientesPorNome(String nomeDePesquisa){
-        List<Cliente> clientes = new ArrayList<>();
-        String sql = "SELECT codigo, nomeCliente, nomeProduto, data, valor "
-                + "FROM Cliente "
-                + "WHERE nomeCliente like CONCAT(?,'%')";
-        try{
-            Connection conexao = FabricaDeConexao.getConexao();
-            if (conexao == null){
-                System.out.println("Sem conexao");
-                System.exit(0);
-            }
-            PreparedStatement stmt = conexao.prepareStatement(sql);
-            stmt.setString(1, nomeDePesquisa);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){
-                int codigo = rs.getInt("codigo");
-                String nomeCliente = rs.getString("nomeCliente");
-                String nomeProduto = rs.getString("nomeProduto");
-                int data = rs.getInt("data");
-                double valor = rs.getDouble("valor");
-                Cliente cliente = new Cliente(codigo, nomeCliente, nomeProduto, data, valor);
-                clientes.add(cliente);
-            }
-            conexao.close();
-        }catch (SQLException ex){
-            System.out.println(ex.toString());
-        }
-        return clientes;
-    }       
-    
 }
