988d16e406de16dcff92b91707393c22816bec53
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-11-07 23:36:07 -0300

    ProdutoController.java

--- Projeto2/src/control/ProdutoController.java
+++ Projeto2/src/control/ProdutoController.java
@@ -7,7 +7,7 @@ import model.Produto;
 
 public class ProdutoController {
     
-    public boolean adicionarProduto(int codigo, String nomeProduto, int qtddProduto, int dataEntrada, double valor){       
+    public boolean adicionarProduto(int codigo, String nomeProduto, int qtddProduto, long dataEntrada, double valor){       
         Produto produto = new Produto(codigo, nomeProduto, qtddProduto, dataEntrada, valor);
         return produto.cadastrar();
     }
@@ -21,24 +21,9 @@ public class ProdutoController {
             dadosProduto[1] = produto.getNomeProduto();
             dadosProduto[2] = String.valueOf(produto.getQtddProduto());
             dadosProduto[3] = String.valueOf(produto.getDataEntrada());
-            dadosProduto[4] = String.valueOf(produto.getValor());
+            dadosProduto[4] = String.valueOf(produto.getValorProduto());
             dadosProdutos.add(dadosProduto);
         }        
         return dadosProdutos;
-    }
-    
-    public List<String[]> listarProdutosPorNome(String nomeProduto){
-        List<String[]> dadosProdutos = new ArrayList<>();
-        List<Produto> produtos = Produto.listarProdutosPorNome(nomeProduto);        
-        for (Produto produto : produtos){
-            String[] dadosProduto = new String[5];
-            dadosProduto[0] = String.valueOf(produto.getCodigo());
-            dadosProduto[1] = produto.getNomeProduto();
-            dadosProduto[2] = String.valueOf(produto.getQtddProduto());
-            dadosProduto[3] = String.valueOf(produto.getDataEntrada());
-            dadosProduto[4] = String.valueOf(produto.getValor());
-            dadosProdutos.add(dadosProduto);
-        }        
-        return dadosProdutos;
-    }    
+    }  
 }
