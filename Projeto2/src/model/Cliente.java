dac1f25130327d790b0e9048ded52976393970f8
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-11-08 02:40:05 -0300

    Cliente.java

--- Projeto2/src/model/Cliente.java
+++ Projeto2/src/model/Cliente.java
@@ -1,17 +1,30 @@
 package model;
 
+import java.util.List;
+import dao.ClienteDAO;
+
 public class Cliente {
 	
+	private int codigo;
 	private String nomeCliente;
 	private String nomeProduto;
-	private int data;
-	private double valor;
+	private long dataCompra;
+	private double valorProduto;
 	
-	public Cliente (String nomeCliente, String nomeProduto, int data, double valor) {
+	public Cliente (int codigo, String nomeCliente, String nomeProduto, long dataCompra, double valorProduto) {
+		this.codigo = codigo;
 		this.nomeCliente = nomeCliente;
 		this.nomeProduto = nomeProduto;
-		this.data = data;
-		this.valor = valor;
+		this.dataCompra = dataCompra;
+		this.valorProduto = valorProduto;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
 	}
 
 	public String getNomeCliente() {
@@ -22,28 +35,44 @@ public class Cliente {
 		this.nomeCliente = nomeCliente;
 	}
 
-	public String getDescricaoProduto() {
+	public String getNomeProduto() {
 		return nomeProduto;
 	}
 
-	public void setDescricaoProduto(String descricaoProduto) {
-		this.nomeProduto = descricaoProduto;
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
 	}
 
-	public int getData() {
-		return data;
+	public long getDataCompra() {
+		return dataCompra;
 	}
 
-	public void setData(int data) {
-		this.data = data;
+	public void setDataCompra(long dataCompra) {
+		this.dataCompra = dataCompra;
 	}
 
-	public double getValor() {
-		return valor;
+	public double getValorProduto() {
+		return valorProduto;
 	}
 
-	public void setValor(double valor) {
-		this.valor = valor;
+	public void setValorProduto(double valorProduto) {
+		this.valorProduto = valorProduto;
 	}
 	
+	@Override
+    public String toString() {
+        return "Cliente{" + "Codigo= " + codigo + 
+        		", Nome do cliente=" + nomeCliente +
+        		",Produto=" + nomeProduto + 
+        		", Data de compra=" + dataCompra + 
+        		", Valor do produto=" + valorProduto + '}';
+    }
+    
+    public boolean cadastrar(){
+        return ClienteDAO.adicionarCliente(this);
+    }
+
+	public static List<Cliente> listarClientes() {
+		return ClienteDAO.listarClientes();
+	}
 }
