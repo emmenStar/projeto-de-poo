0680c16e79d122b7ed111da394b5bb854a0ec6c9
Eduarda Miranda <113375327+mirandaedu@users.noreply.github.com>
2023-11-07 23:35:34 -0300

    ClienteController.java

--- Projeto2/src/control/ClienteController.java
+++ Projeto2/src/control/ClienteController.java
@@ -7,38 +7,23 @@ import model.Cliente;
 
 public class ClienteController {
     
-    public boolean adicionarCliente(int codigo, String nomeCliente, String nomeProduto, int data, double valor){       
-        Cliente cliente = new Cliente(codigo, nomeCliente, nomeProduto, data, valor);
+    public boolean adicionarCliente(int codigo, String nomeCliente, String nomeProduto, long dataCompra, double valorProduto){       
+        Cliente cliente = new Cliente(codigo, nomeCliente, nomeProduto, dataCompra, valorProduto);
         return cliente.cadastrar();
     }
     
     public List<String[]> listarClientes(){
         List<String[]> dadosClientes = new ArrayList<>();
-        List<Cliente> clientes = Cliente.listarCliente();        
+        List<Cliente> clientes = Cliente.listarClientes();        
         for (Cliente cliente : clientes){
             String[] dadosCliente = new String[4];
             dadosCliente[0] = String.valueOf(cliente.getCodigo());
             dadosCliente[1] = cliente.getNomeCliente();
             dadosCliente[2] = cliente.getNomeProduto();
-            dadosCliente[3] = String.valueOf(cliente.getData());
-            dadosCliente[4] = String.valueOf(cliente.getValor());
+            dadosCliente[3] = String.valueOf(cliente.getDataCompra());
+            dadosCliente[4] = String.valueOf(cliente.getValorProduto());
             dadosClientes.add(dadosCliente);
         }        
         return dadosClientes;
-    }
-    
-    public List<String[]> listarClientesPorNome(String nomeCliente){
-        List<String[]> dadosClientes = new ArrayList<>();
-        List<Cliente> clientes = Cliente.listarClientesPorNome(nomeCliente);        
-        for (Cliente cliente : clientes){
-            String[] dadosCliente = new String[4];
-            dadosCliente[0] = String.valueOf(cliente.getCodigo());
-            dadosCliente[1] = cliente.getNomeCliente();
-            dadosCliente[2] = cliente.getNomeProduto();
-            dadosCliente[3] = String.valueOf(cliente.getData());
-            dadosCliente[4] = String.valueOf(cliente.getValor());
-            dadosClientes.add(dadosCliente);
-        }        
-        return dadosClientes;
-    }    
+    } 
 }
